cd0535a4724f1589beb6c02d0b5c2a1073f1ddc5
Katrina Guyduy <katrinaguyduy@gmail.com>
2015-10-30 16:40:35 -0700

    basic css

--- wp-content/themes/leredbread/header.php
+++ wp-content/themes/leredbread/header.php
@@ -7,11 +7,12 @@
 
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-	<head>
+		<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
 	<?php wp_head(); ?>
 	</head>
@@ -22,13 +23,26 @@
 
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-				</div><!-- .site-branding -->
+					<div class="site-branding-inner contain">
+
+<div class="site-branding-inner contain">
+						<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+						<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+						 <a href="" rel="home"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/images/lrb-logo.svg" alt="Logo"/>
+					</a></div>
+				</div>
+
+
+
+
+
+
+<!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+				<div class="header-search"><?php get_search_form(); ?></div>
 				</nav><!-- #site-navigation -->
 			</header><!-- #masthead -->
 
