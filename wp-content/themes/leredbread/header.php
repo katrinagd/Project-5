9a07532744c68ba04fc60bbaa2df5c11acf086e5
Katrina Guyduy <katrinaguyduy@gmail.com>
2015-11-13 11:27:45 -0800

    active menu item fix

--- wp-content/themes/leredbread/header.php
+++ wp-content/themes/leredbread/header.php
@@ -29,7 +29,8 @@
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<!-- <p class="site-description"><?php bloginfo( 'description' ); ?></p> -->
 						<div class="logo">
-							<img class="logo" src="<?php echo get_template_directory_uri(); ?>/images/lrb-logo.svg"/>
+							
+							<a href="<?php echo esc_url(home_url( '/')); ?>" rel="home"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/images/lrb-logo.svg"/></a>
 						</div>
 
 						<div >
